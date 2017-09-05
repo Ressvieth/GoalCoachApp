ad3d81455609e7c02c36b11bf1c134da899830e5
Piotr Tylicki <piotr.tylicki@gmail.com>
2017-09-05 16:39:15 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+// Here goes authentication data from firebase //
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+
+export const firebaseApp = firebase.initializeApp(config);
+export const goalRef = firebase.database().ref('goals');
+export const completeGoalRef = firebase.database().ref('completeGoals');
